bfe868596d66e1bc750529f31fb435c5276a8d86
Basile Pesin <basile.pesin@gmail.com>
2019-12-02 16:43:25 +0100

    Basic setup for the project

---
+++ test/clj_lprolog/core.clj
@@ -0,0 +1,8 @@
+(ns clj-lprolog.core
+  (:require [clj-lprolog.core :as sut]
+            [clojure.test :as t]))
+
+(t/deftest dummy-test
+  (t/testing "Dummy test."
+    (t/is (= 1 1))
+    (t/is (not= 0 1))))
